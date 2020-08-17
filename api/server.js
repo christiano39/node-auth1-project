6dff5b7298dccc5e1830adc0a1ad4b53f2cd5084
christiano39 <carneson39@gmail.com>
2020-08-17 13:49:01 -0600

    implemented protected mw

--- api/server.js
+++ api/server.js
@@ -4,13 +4,14 @@ const sessionConfig = require("../session/session-config");
 
 const usersRouter = require("../users/users-routes");
 const authRouter = require("../auth/auth-routes");
+const protected = require("../auth/protected-mw");
 
 const server = express();
 
 server.use(express.json());
 server.use(session(sessionConfig));
 
-server.use("/api/users", usersRouter);
+server.use("/api/users", protected, usersRouter);
 server.use("/api/auth", authRouter);
 
 module.exports = server;
--- auth/auth-routes.js
+++ auth/auth-routes.js
@@ -32,4 +32,41 @@ router.post("/register", (req, res) => {
     });
 });
 
+router.post("/login", (req, res) => {
+  let { username, password } = req.body;
+
+  if (!username || !password) {
+    res
+      .status(400)
+      .json({ message: "Please provide required username and password" });
+  }
+
+  Users.findBy({ username })
+    .then(([user]) => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        req.session.loggedIn = true;
+        res.status(200).json({ hello: user.username, session: req.session });
+      } else {
+        res.status(401).json({ error: "Invalid credentials" });
+      }
+    })
+    .catch((err) => {
+      require.status(500).json({ error: err.message });
+    });
+});
+
+router.get("/logout", (req, res) => {
+  if (req.session) {
+    req.session.destroy((err) => {
+      if (err) {
+        res.status(500).json({ error: "Could not log out" });
+      } else {
+        res.status(204).end();
+      }
+    });
+  } else {
+    res.status(200).json({ message: "Already logged out" });
+  }
+});
+
 module.exports = router;
